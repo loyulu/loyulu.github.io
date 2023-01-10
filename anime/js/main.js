3a1974d57477f92bf4b5b644edb4fe7bd0bb0dac
loyulu <121545762+loyulu@users.noreply.github.com>
2023-01-11 00:11:07 +0800

    login adjust

--- anime/js/main.js
+++ anime/js/main.js
@@ -14,6 +14,13 @@ $(".more_btn_1").click(function () {
   $(".new_more").slideToggle();
 });
 $(".more_btn_2").click(function () {
-  $("again_more").slideToggle();
+  $(".again_more").slideToggle();
 });
-//
+$(".enter").click(function () {
+  var inputVal = $('input[name="memberName"]').val();
+  var inputVal2 = $('input[name="pass"]').val();
+  console.log(inputVal, inputVal2);
+  if (inputVal2 !== "456") {
+    alert("密碼錯誤");
+  }
+});
