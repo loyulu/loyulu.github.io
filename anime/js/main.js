9abb0cfdd042e961f8161a37fcc382c01b3ec22c
loyulu <121545762+loyulu@users.noreply.github.com>
2023-01-10 19:04:10 +0800

    new card

--- anime/js/main.js
+++ anime/js/main.js
@@ -10,3 +10,10 @@ $(".logIn_close").click(function () {
   $("#logIn").hide();
   $(".backdrop").hide();
 });
+$(".more_btn_1").click(function () {
+  $(".new_more").slideToggle();
+});
+$(".more_btn_2").click(function () {
+  $("again_more").slideToggle();
+});
+//
